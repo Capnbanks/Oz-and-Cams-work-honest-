eaa5b3faeed09408b313bcc7445b7a50ecdba4bf
Capnbanks <144108761+Capnbanks@users.noreply.github.com>
2024-03-10 18:53:39 +1300

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,1 @@
-const proxy = require('http-proxy-middleware');
 
-const apiUrl = 'http://localhost:51357';
-
-module.exports = (app) => {
-    app.use(
-        proxy('/api', {
-            target: apiUrl,
-            changeOrigin: true,
-        })
-    );
-};
